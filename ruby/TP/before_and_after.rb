ce3f49f68b24f01ab3eb9403c28e1ddc0a82c34a
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-04-24 08:56:30 -0300

    Resuelvo problema de llamar a mas de un before_and_after dentro de una misma clase

--- ruby/TP/before_and_after.rb
+++ ruby/TP/before_and_after.rb
@@ -1,18 +1,36 @@
 class Object
   def self.before_and_after_each_call(before, after)
     @overriden_methods = []
+    @befores ||= []
+    @befores.push(before)
+    @afters ||= []
+    @afters.push(after)
     self.define_singleton_method :method_added do |method|
       if !@overriden_methods.include? method
         @overriden_methods.push method
         aux = self.new.method(method) #unbound
         define_method method do |*args|
-          before.call
+          self.class.instance_eval do
+            @befores.each{|p| p.call}
+          end
           aux.call(*args)
-          after.call
+          self.class.instance_eval do
+            @afters.each{|p| p.call}
+          end
         end
       end
     end
   end
 end
+
+
+class Ejemplo
+  before_and_after_each_call(proc {puts "Estoy entrando"}, proc {puts "Estoy saliendo"})
+  before_and_after_each_call(proc {puts "Estoy entrandoaa"}, proc {puts "Estoy saliendoaaa"})
+  def saludar
+    puts "Entre"
+  end
+end
+
 
 
