0e08c41c70eb8fffe9ca0a4bf9a83d74b552ec63
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-04-26 17:31:09 -0300

    Agrego comentarios para hacerlo entendible

--- ruby/TP/before_and_after+invariantes.rb
+++ ruby/TP/before_and_after+invariantes.rb
@@ -1,45 +1,44 @@
 class Object
   def self.invariante(&condicion)
-    @invariantes ||= []
-    @invariantes.push(condicion)
+    @invariantes ||= [] # inicializo por default como "[]"
+    @invariantes.push(condicion) # agrego proc "condicion"
   end
 
   def chequear_invariantes
-    invariantes = self.class.instance_variable_get :@invariantes
-    invariantes ||= []
-    if !invariantes.all? {|condicion| instance_eval &condicion}
-      raise "No se cumplen todas las invariantes!!!"
-    else
-      true
+    invariantes = self.class.instance_variable_get :@invariantes # obtengo la lista de invariantes
+    if !invariantes.all? {|condicion| instance_eval &condicion} # verifico que se cumplan todas las invariantes
+      raise "No se cumplen todas las invariantes!!!" # en caso de que no se cumplan tiro una excepcion
     end
   end
 
   def self.before_and_after_each_call(before, after)
-    @overriden_methods = [:initialize]
-    @befores ||= []
-    @befores.push(before)
-    @afters ||= []
-    @afters.push(after)
+    @overriden_methods = [:initialize] # inicializo. Incluyo "initialize": algunas invariantes involucran atributos que se inicializan en este metodo. Evito que initialize se redefina para que no ocurra un error al tratar con atributos dentro de invariantes no inicializados.
+    @befores ||= [] # inicializo por default como "[]".
+    @befores.push(before) # agrego proc "before"
+    @afters ||= [] # inicializo por default como "[]"
+    @afters.push(after) # agrego proc "after"
     self.define_singleton_method :method_added do |method|
-      if !@overriden_methods.include? method
-        @overriden_methods.push method
-        aux = self.instance_method(method) #unbound
-        define_method method do |*args|
-          self.class.instance_variable_get(:@befores).reverse_each{|p| instance_eval &p}
-          retorno = aux.bind(self).call(*args)
-          self.class.instance_variable_get(:@afters).each{|p| instance_eval &p}
-          retorno
+      if !@overriden_methods.include? method # control para que haya un bucle de definicion y redefinicion infinito
+        @overriden_methods.push method # idem ^^^
+        aux = self.instance_method(method) #unbound. Guardo el metodo original
+        define_method method do |*args| # redefino el metodo original con el mismo nombre y sus argumentos
+          instance_eval {self.send(:chequear_invariantes)}  # chequeo de invariantes a priori
+          self.class.instance_variable_get(:@befores).reverse_each{|p| instance_eval &p} # evaluo todos los procs "before" dentro del contexto de la instancia correspondiente
+          retorno = aux.bind(self).call(*args) # bindeo y ejecuto el metodo original; guardo el valor de retorno
+          self.class.instance_variable_get(:@afters).each{|p| instance_eval &p} # evaluo todos los procs "after" dentro del contexto de la instancia correspondiente
+          instance_eval {self.send(:chequear_invariantes)} # chequeo de invariantes a posteriori
+          retorno # retorno el retorno original
         end
       end
     end
   end
 end
 
+# un ejemplo para testear
 class Ejemplo
   attr_accessor :atributo
 
   before_and_after_each_call(proc {puts "Estoy entrando"}, proc {puts "Estoy saliendo"})
-  before_and_after_each_call(proc{send :chequear_invariantes}, proc{send :chequear_invariantes})
 
   def initialize
     self.atributo = 3
