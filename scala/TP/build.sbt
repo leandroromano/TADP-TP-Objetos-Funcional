0152ea62fab4c1d456facedf522dd80fbac7f44b
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-06-14 00:50:05 -0300

    Idea base

---
+++ scala/TP/build.sbt
@@ -0,0 +1,7 @@
+name := "tp-scala-2019-1c"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ scala/TP/src/main/scala/MusicParser.scala
@@ -0,0 +1,49 @@
+import java.io.{PushbackReader, StringReader}
+import Musica._
+import scala.collection.mutable.ListBuffer
+
+case class Note(name: String)
+
+class MusicParser(input: String) {
+  protected val inputStream = new PushbackReader(new StringReader(input))
+
+  protected def parseChar(): Char = {
+    val parsed = inputStream.read()
+    if (parsed == -1) throw new EOIParserException
+    return parsed.toChar
+  }
+
+  protected def parseNote(): Nota = {
+    var next: Char = ' '
+    do next = parseChar() while (next == ' ')
+    Nota.notas.find(_.toString == next.toString()).getOrElse(throw new NotANoteException(next))
+  }
+
+  def parse(): List[Nota] = {
+    val result: ListBuffer[Nota] = ListBuffer()
+    try while (true)
+      result += parseNote()
+    catch {
+      case _: EOIParserException =>
+    }
+    return result.toList
+  }
+}
+
+class ParserException(reason: String) extends Exception(reason)
+class EOIParserException extends ParserException("reached end of input")
+class NotANoteException(val read: Char) extends ParserException(s"Expected [A|B|C|D|E|F|G] but got $read")
+
+trait Exprecion {
+  def toPartitura: List[Nota]
+}
+case class Multiplicacion(multiplicador: Integer, contenido: Exprecion){
+  def toPartitura: List[Nota] = {
+    var aux: List[Nota] = List()
+    (1 to multiplicador).foreach{ _ => aux = aux ::: contenido.toPartitura }
+    return aux
+  }
+}
+case class Simple(contenido: List[Nota]){
+  def toPartitura: List[Nota] = contenido
+}
